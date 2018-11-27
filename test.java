3870b3654db6739b914812ae6687f307ffe23587
LeBronJJJ <243177325@qq.com>
2018-11-27 23:47:47 +0800

    send

---
+++ test.java
@@ -0,0 +1,29 @@
+public class test{
+	public static void main(String[] args){
+		/*int num = 2;		//定義一個整形變量
+		int maxValue = Integer.MAX_VALUE;
+		int minValue = Integer.MIN_VALUE;
+		long num1 = maxValue + 1;
+		long num2 = minValue - 1;
+		long num0 = 214748364123132L;
+		int res = (int)num0;		//强制類型轉換
+		System.out.println(num * 2);
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num0);
+		System.out.println(res);
+		*/
+		/*
+		int num;
+		System.out.println(num);
+		*/								//記得先初始化
+
+		Test test = new Test();
+		System.out.println(test.a);
+
+	}
+}
+
+class Test{
+	public int a = 200;
+}
