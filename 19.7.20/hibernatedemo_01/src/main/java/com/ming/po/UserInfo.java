ab617a62b90d35f077b00df3bd2ed47243d63ff5
LebronJJJ <243177325@qq.com>
2019-07-20 22:49:50 +0800

    Hibernate 的使用方法

---
+++ 19.7.20/hibernatedemo_01/src/main/java/com/ming/po/UserInfo.java
@@ -0,0 +1,39 @@
+package com.ming.po;
+
+public class UserInfo {
+
+    // 必须的
+    private int userId;
+
+    private String userName;
+
+    private String userPass;
+
+    public UserInfo(){
+
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+}
